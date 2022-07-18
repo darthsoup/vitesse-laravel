f866a92148835741b30e7b87eed362b2a2fee130
Kevin Krummnacker <kevin.krummnacker@gmail.com>
2022-07-19 01:08:29 +0200

    feat: add more unplugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import Laravel from 'laravel-vite-plugin'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 import Vue from '@vitejs/plugin-vue'
+import VueI18n from '@intlify/vite-plugin-vue-i18n'
+import Inspect from 'vite-plugin-inspect'
 import Unocss from 'unocss/vite'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+
   plugins: [
     Laravel({
       input: 'resources/js/app.ts',
@@ -20,8 +31,47 @@ export default defineConfig({
       reactivityTransform: true,
     }),
 
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-i18n',
+        '@vueuse/head',
+        '@vueuse/core',
+      ],
+      dts: 'resources/js/auto-imports.d.ts',
+      dirs: [
+        'resources/js/Composables',
+        'resources/js/Store',
+      ],
+      vueTemplate: true,
+    }),
+
+    // https://github.com/antfu/unplugin-vue-components
+    Components({
+      // relative paths to the directory to search for components.
+      dirs: ['resources/js/components'],
+
+      // allow auto load markdown components under `./src/components/`
+      extensions: ['vue', 'md'],
+      // allow auto import and register components used in markdown
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      dts: 'resources/js/components.d.ts',
+    }),
+
     // https://github.com/antfu/unocss
     // see unocss.config.ts for config
     Unocss(),
+
+    // https://github.com/intlify/bundle-tools/tree/main/packages/vite-plugin-vue-i18n
+    VueI18n({
+      runtimeOnly: true,
+      compositionOnly: true,
+      include: [path.resolve(__dirname, 'locales/**')],
+    }),
+
+    // https://github.com/antfu/vite-plugin-inspect
+    // Visit http://localhost:3333/__inspect/ to see the inspector
+    Inspect(),
   ],
 })
