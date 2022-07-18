37b23e35a785e9c4f4177087df62c8dced5d8798
Kevin Krummnacker <kevin.krummnacker@gmail.com>
2022-07-19 00:51:03 +0200

    feat: basic vitesse setup with unocss

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,27 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import Laravel from 'laravel-vite-plugin'
+import Vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-});
+  plugins: [
+    Laravel({
+      input: 'resources/js/app.ts',
+      refresh: true,
+    }),
+
+    Vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+      reactivityTransform: true,
+    }),
+
+    // https://github.com/antfu/unocss
+    // see unocss.config.ts for config
+    Unocss(),
+  ],
+})
